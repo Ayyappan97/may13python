7e1c0e8959732935aac932429bb3fc897a52db2d
Ayyappanhsr97 <ayyappanhsr97@gmail.com>
2020-05-14 00:10:56 +0530

    condition statement practise

---
+++ loop practice.py
@@ -0,0 +1,52 @@
+
+# for loop exercise
+"""
+prices = range(10)
+total = 0
+for item in prices:
+    total += item
+print(f"total items:{total}")
+"""
+
+#nested loop
+"""
+for x in range(4):
+    for y in range(3):
+        print(f"{x},{y}")
+"""
+#nested exercise
+#to print L using nested loop
+"""
+numbers = [1,1,1,1,4]
+for num in numbers:
+    output = ""
+    for x in range(num):
+        output += "*"
+    print(output)
+"""
+
+#largest number
+'''
+numbers = ['10','20','30','40','50','60']
+max = numbers[0]
+for num in numbers:
+    if num > max:
+        max = num
+print(max)
+'''
+#square number
+"""
+def num (number):
+    return number * number
+value = num(5)
+print(value)
+"""
+
+
+
+
+
+
+
+
+
