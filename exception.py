6eca3e94c79306d09a5e55c392f7a93b885e1dbe
Ayyappanhsr97 <ayyappanhsr97@gmail.com>
2020-05-14 00:09:22 +0530

    exception handling practise

---
+++ exception.py
@@ -0,0 +1,17 @@
+# exception handling
+'''
+try:
+   age = int(input("whats your age:"))
+   print(f'your age is {age}')
+except ValueError:
+    print('invalid format')
+'''
+"""
+try:
+    age=0
+    salary=2000
+    total = salary/age
+except ZeroDivisionError:
+    print('invalid')
+
+"""
